b5768c6abec72ab2c8ab4535a1ee07920d5fb308
israel betancourt <betancourt.pi@outllook.com>
2022-03-12 15:01:59 -0600

    20220312 -Base project ADACE_Norte_Centro.Consola

---
+++ ADACE_Norte_Centro.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADACE_Norte_Centro.Consola
+{
+    public class Consola
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a ADACE Norte-Centro");
+        }
+    }
+}
