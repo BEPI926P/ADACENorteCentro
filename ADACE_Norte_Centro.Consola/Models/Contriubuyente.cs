a09878001ed6b7942d65226c70d79e3df2516e30
Israel Betancourt Puente <betancourt.pi@outlool.com>
2022-03-14 19:11:36 -0600

    20220314
    CambiosVersion.net y Contribuyente

---
+++ ADACE_Norte_Centro.Consola/Models/Contriubuyente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ADACE_Norte_Centro.Consola.Models
+{
+    public class Contriubuyente { 
+
+        public string idContribuyente { get; set; }
+        public string NombredeContribuyente {get; set;}
+        public string RFC { get; set; }
+        public string GiroContribuyente { get; set; }
+        public string DomicilioFiscal { get; set; }
+        public string DomicilioParaOiryRecibirNotificaciones { get; set; }
+        public Contriubuyente()
+        {
+        }
+    }
+}
--- ADACE_Norte_Centro.Consola/Program.cs
+++ ADACE_Norte_Centro.Consola/Program.cs
@@ -12,12 +12,18 @@ namespace ADACE_Norte_Centro.Consola
         public static void Paso2()
         {
             Console.WriteLine("Bienvenido a ADACE Norte-Centro");
-            string connetionString = "Server=localhost;Database=auditoria;Uid=adace;Pwd=adaceNC";
+            string connectionString = "Server=localhost;Database=auditoria;Uid=adace;Pwd=adaceNC";
 
             // Probar conexion a la base de datos
-            DBConnection.DBConnection dBConnection = new DBConnection.DBConnection(connetionString);
+            DBConnection.DBConnection dBConnection = new DBConnection.DBConnection(connectionString);
             dBConnection.OpenConnection();
             dBConnection.Consulta("SELECT * FROM auditoria.expediente;");
+
+
+            //Conexion nueva
+            DBConnection.DBConnection dBConnection1 = new DBConnection.DBConnection(connectionString);
+            dBConnection1.OpenConnection();
+            dBConnection1.Consulta("SELECT * FROM Auditoria.Contribuyente;");
         }
     }
 }
