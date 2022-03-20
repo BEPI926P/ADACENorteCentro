30c45cb300672eb49662d3eb7dd19810005fb772
israel betancourt <betancourt.pi@outllook.com>
2022-03-20 14:53:28 -0600

    20220320
    - Leer dato.

--- Auditoria_Preventiva/Program.cs
+++ Auditoria_Preventiva/Program.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.IO;
 
 namespace Auditoria_Preventiva
 {
     public class Auditoria_Preventiva
     {
         public static void Main(string[] args)
+        {
+            // pasos para procesar nuestro archivo txt en excel
+            // * agregar log
+            // - Leer archivo .asc (leemos como txt normal) y cargamos la informacion.
+            // - Procesamos los datos leidos y convertimos a datos requeridos.
+            // - Vacias a excel nuestros datos procesados.
+            string path = string.Empty;
+            Console.Write("Ingresa la ubicacion de la carpeta a leer: ");
+
+            Paso_1();
+            Paso_2();
+            Paso_3();
+        }
+
+        public static void Paso_1(string path)
+        {
+            string[] files = Directory.GetFiles(path, "*.txt");
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        public static void Paso_2()
+        {
+
+        }
+
+        public static void Paso_3()
         {
 
         }
