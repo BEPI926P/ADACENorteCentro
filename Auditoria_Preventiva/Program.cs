b3322c5f1411d801c9685ea1dd48b44025643ae3
David Mendez <demg@outlook.com>
2022-03-23 19:24:49 -0600

    20220323 - Add all the code to read a txt file.

--- Auditoria_Preventiva/Program.cs
+++ Auditoria_Preventiva/Program.cs
@@ -14,19 +14,56 @@ namespace Auditoria_Preventiva
             // - Vacias a excel nuestros datos procesados.
             string path = string.Empty;
             Console.Write("Ingresa la ubicacion de la carpeta a leer: ");
+            path = Console.ReadLine();
 
-            Paso_1();
+            Paso_1(path);
             Paso_2();
             Paso_3();
         }
 
-        public static void Paso_1(string path)
+        public static void ListarDirectorio(string path, string fileExtension)
         {
-            string[] files = Directory.GetFiles(path, "*.txt");
+            string[] files = Directory.GetFiles(path, fileExtension);
             foreach (var file in files)
             {
                 Console.WriteLine(file);
             }
+        }
+
+        public static void Paso_1_REF(string path)
+        {
+            // Listar un directorio
+            ListarDirectorio(path, "*.*");
+
+            // Leer archivo de texto
+            string file = System.IO.File.ReadAllText(path + "/SWH.usp_Reports_GetPackaginSlip.sql");
+
+            // Modificamos archivo de texto
+            string file2 = file.Replace("/* rptPackaginSlip */", "/* rpt */");
+
+            // Imprimimos en pantalla el archivo modificado
+            System.Console.WriteLine(file2);
+
+            // Creamos un nuevo archivo
+            File.WriteAllText(path + "/SWH.usp_Reports_GetPackaginSlip_MODIFICADO.sql", file2);
+        }
+
+        public static void Paso_1(string path)
+        {
+            // Leer archivo de texto
+            string[] fileLines = File.ReadAllLines(path + "/SWH.usp_Reports_GetPackaginSlip.sql");
+
+            // Modificamos archivo de texto
+            fileLines[0] = "He modificado la linea";
+
+            // Imprimimos en pantalla el archivo modificado
+            foreach (string line in fileLines)
+            {
+                Console.WriteLine(line);
+            }
+
+            // Creamos un nuevo archivo
+            File.WriteAllLines(path + "/SWH.usp_Reports_GetPackaginSlip_MODIFICADO.sql", fileLines);
         }
 
         public static void Paso_2()
